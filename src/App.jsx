c10799cb3a5b66e29240f2a8895ae59a8da9731d
Daniel <d.napieraj06@gmail.com>
2025-07-14 22:17:58 +0200

    enhance place removal functionality and improve accessibility in confirmation dialog

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import Modal from './components/Modal.jsx'
 import DeleteConfirmation from './components/DeleteConfirmation.jsx'
 import logoImg from './assets/logo.png'
 import { sortPlacesByDistance } from './loc.js'
+import { use } from 'react'
+
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find(place => place.id === id))
 
 function App() {
 	const modal = useRef()
 	const selectedPlace = useRef()
 	const [availablePlaces, setAvailablePlaces] = useState([])
-	const [pickedPlaces, setPickedPlaces] = useState([])
+	const [pickedPlaces, setPickedPlaces] = useState(storedPlaces.length > 0 ? storedPlaces : [])
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(
@@ -53,6 +57,8 @@ function App() {
 	function handleRemovePlace() {
 		setPickedPlaces(prevPickedPlaces => prevPickedPlaces.filter(place => place.id !== selectedPlace.current))
 		modal.current.close()
+		const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+		localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter(id => id !== selectedPlace.current)))
 	}
 
 	return (
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,16 +1,23 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  return (
-    <div id="delete-confirmation">
-      <h2>Are you sure?</h2>
-      <p>Do you really want to remove this place?</p>
-      <div id="confirmation-actions">
-        <button onClick={onCancel} className="button-text">
-          No
-        </button>
-        <button onClick={onConfirm} className="button">
-          Yes
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			id="delete-confirmation"
+			tabIndex={0}
+			onKeyDown={e => {
+				if (e.key === 'Enter') {
+					onConfirm()
+				}
+			}}>
+			<h2>Are you sure?</h2>
+			<p>Do you really want to remove this place?</p>
+			<div id="confirmation-actions">
+				<button onClick={onCancel} className="button-text">
+					No
+				</button>
+				<button onClick={onConfirm} className="button">
+					Yes
+				</button>
+			</div>
+		</div>
+	)
 }
