ad163a3a54b71d3f3592c6e0ce2e24714a0c8255
Daniel <d.napieraj06@gmail.com>
2025-07-19 13:24:15 +0200

    refactor modal and delete confirmation components to improve handling and user experience

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,20 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import { createPortal } from 'react-dom'
 
 const Modal = function Modal({ open, children }) {
 	const dialog = useRef()
 
-	dialog.current.showModal()
+	useEffect(() => {
+		if (open) {
+			dialog.current.showModal()
+		} else {
+			dialog.current.close()
+		}
+	}, [open])
 
 	return createPortal(
-		<dialog className="modal" ref={dialog} >
-			{children}
+		<dialog className="modal" ref={dialog}>
+			{open ? children : null}
 		</dialog>,
 		document.getElementById('modal')
 	)
